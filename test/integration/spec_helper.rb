fa1bd6e4fe39993a642e2f70ef30852536d480fd
Simon Detheridge <simon@widgit.com>
2016-07-08 17:44:32 +0100

    Fix long line in integration spec

--- test/integration/spec_helper.rb
+++ test/integration/spec_helper.rb
@@ -12,6 +12,7 @@ end
 RSpec.configure do |config|
   config.before(:each) do
     integration_mysql_client.query('DELETE FROM accounts WHERE account_name="integration";')
-    integration_mysql_client.query('INSERT INTO accounts (account_name,account_key) VALUES ("integration","x");')
+    integration_mysql_client.query('INSERT INTO accounts (account_name,account_key) '\
+                                   'VALUES ("integration","x");')
   end
 end
