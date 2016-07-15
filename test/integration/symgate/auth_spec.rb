805270ea9d44628485c88d4cbf4a234d3c2ec80b
Simon Detheridge <simon@widgit.com>
2016-07-15 11:58:40 +0100

    Add tests for create_user

--- test/integration/symgate/auth_spec.rb
+++ test/integration/symgate/auth_spec.rb
@@ -9,6 +9,14 @@ def account_key_client
                             savon_opts: savon_opts)
 end
 
+def user_password_client(user, password)
+  Symgate::Auth::Client.new(account: 'integration',
+                            user: user,
+                            password: password,
+                            endpoint: 'http://localhost:11122/',
+                            savon_opts: savon_opts)
+end
+
 RSpec.describe(Symgate::Auth::Client) do
   describe '#enumerate_groups' do
     it 'returns an empty array if there are no groups' do
@@ -133,5 +141,72 @@ RSpec.describe(Symgate::Auth::Client) do
       users.each { |u| expect { client.create_user(u, 'password') }.not_to raise_error }
       expect(client.enumerate_users('foo')).to eq(users)
     end
+  end
+
+  describe '#create_user' do
+    it 'should raise an error if the user id does not contain a group' do
+      client = account_key_client
+
+      expect { client.create_user(Symgate::Auth::User.new(user_id: 'fnar'), 'asdf1234') }
+        .to raise_error(Symgate::Error)
+    end
+
+    it 'should raise an error if the user id within the group is invalid' do
+      client = account_key_client
+
+      expect { client.create_group('foo') }.not_to raise_error
+
+      expect { client.create_user(Symgate::Auth::User.new(user_id: 'foo/bar/baz'), 'asdf1234') }
+        .to raise_error(Symgate::Error)
+    end
+
+    it 'should raise an error if the group does not exist' do
+      client = account_key_client
+
+      expect { client.create_user(Symgate::Auth::User.new(user_id: 'foo/bar'), 'asdf1234') }
+        .to raise_error(Symgate::Error)
+    end
+
+    it 'should create a user when called with correct parameters' do
+      client = account_key_client
+      user = Symgate::Auth::User.new(user_id: 'foo/bar')
+
+      expect { client.create_group('foo') }.not_to raise_error
+
+      expect { client.create_user(user, 'asdf1234') }.not_to raise_error
+
+      expect(client.enumerate_users('foo')).to eq([user])
+    end
+
+    it 'should create a group admin user when requested to do so' do
+      client = account_key_client
+      user = Symgate::Auth::User.new(user_id: 'foo/bar', is_group_admin: true)
+
+      expect { client.create_group('foo') }.not_to raise_error
+      expect { client.create_user(user, 'asdf1234') }.not_to raise_error
+      expect(client.enumerate_users('foo')).to eq([user])
+    end
+
+    it 'should raise an error when called with user credentials' do
+      admin_client = account_key_client
+      user_client = user_password_client('foo/bar', 'asdf1234')
+      admin_user = Symgate::Auth::User.new(user_id: 'foo/bar')
+      new_user = Symgate::Auth::User.new(user_id: 'foo/baz')
+
+      expect { admin_client.create_group('foo') }.not_to raise_error
+      expect { admin_client.create_user(admin_user, 'asdf1234') }.not_to raise_error
+      expect(admin_client.enumerate_users('foo')).to eq([admin_user])
+
+      expect { user_client.create_user(new_user, 'asdf1234') }.to raise_error(Symgate::Error)
+    end
+
+    # Bug in upstream. See SOS-202
+    # it 'should raise an error when called with a blank password' do
+    #   client = account_key_client
+    #   user = Symgate::Auth::User.new(user_id: 'foo/bar', is_group_admin: true)
+    #
+    #   expect { client.create_group('foo') }.not_to raise_error
+    #   expect { client.create_user(user, '') }.to raise_error(Symgate::Error)
+    # end
   end
 end
