74df98525ba27c430e2990336e34aff6d55cec20
Simon Detheridge <simon@widgit.com>
2016-08-25 16:32:58 +0100

    Add unit tests for get_metadata

--- test/spec/symgate/metadata/client_spec.rb
+++ test/spec/symgate/metadata/client_spec.rb
@@ -5,36 +5,117 @@ require 'symgate/metadata/client'
 RSpec.describe(Symgate::Auth::Client) do
   describe '#get_metadata' do
     it 'returns an empty array if there are no items' do
+      savon.expects(:get_metadata)
+           .with(message: { 'auth:creds': user_password_creds('foo', 'foo/bar', 'baz') })
+           .returns(File.read('test/spec/fixtures/xml/get_metadata_empty.xml'))
+
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata).to match_array([])
     end
 
     it 'returns an array of one DataItem if there is one item' do
+      savon.expects(:get_metadata)
+           .with(message: { 'auth:creds': user_password_creds('foo', 'foo/bar', 'baz') })
+           .returns(File.read('test/spec/fixtures/xml/get_metadata_one.xml'))
+
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'foo', value: 'bar', scope: 'Account')
+        ]
+      )
     end
 
     it 'returns an array of multiple DataItems if there are several items' do
+      savon.expects(:get_metadata)
+           .with(message: { 'auth:creds': user_password_creds('foo', 'foo/bar', 'baz') })
+           .returns(File.read('test/spec/fixtures/xml/get_metadata_two.xml'))
+
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'foo', value: 'bar', scope: 'Account'),
+          Symgate::Metadata::DataItem.new(key: 'baz', value: 'qux', scope: 'Group')
+        ]
+      )
     end
 
     it 'accepts a scope as an input' do
+      savon.expects(:get_metadata)
+           .with(message: { 'auth:creds': user_password_creds('foo', 'foo/bar', 'baz'),
+                            scope: 'Account' })
+           .returns(File.read('test/spec/fixtures/xml/get_metadata_one.xml'))
+
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(scope: 'Account')).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'foo', value: 'bar', scope: 'Account')
+        ]
+      )
     end
 
     it 'raises an error if an invalid scope is supplied' do
+      savon.expects(:get_metadata)
+           .with(message: { 'auth:creds': user_password_creds('foo', 'foo/bar', 'baz'),
+                            scope: 'Teapot' })
+           .returns(File.read('test/spec/fixtures/xml/generic_error.xml'))
+
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(scope: 'Teapot')).to raise_error(Symgate::Error)
     end
 
     it 'accepts an array of keys as an input' do
+      savon.expects(:get_metadata)
+           .with(message: { 'auth:creds': user_password_creds('foo', 'foo/bar', 'baz'),
+                            keys: %w(foo baz) })
+           .returns(File.read('test/spec/fixtures/xml/get_metadata_two.xml'))
+
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(keys: %w(foo baz))).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'foo', value: 'bar', scope: 'Account'),
+          Symgate::Metadata::DataItem.new(key: 'baz', value: 'qux', scope: 'Group')
+        ]
+      )
     end
 
     it 'accepts a single key as an input' do
+      savon.expects(:get_metadata)
+           .with(message: { 'auth:creds': user_password_creds('foo', 'foo/bar', 'baz'),
+                            key: 'foo' })
+           .returns(File.read('test/spec/fixtures/xml/get_metadata_one.xml'))
+
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(key: 'foo')).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'foo', value: 'bar', scope: 'Account')
+        ]
+      )
     end
 
     it 'raises an error if "keys" is not an array' do
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(keys: 'foo')).to raise_error(Symgate::Error)
     end
 
     it 'raises an error if "keys" contains non-string items' do
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(keys: ['foo', 6])).to raise_error(Symgate::Error)
     end
 
     it 'raises an error if "key" is not a string' do
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(key: nil)).to raise_error(Symgate::Error)
+    end
+
+    it 'raises an error if both "key" and "keys" are supplied' do
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(key: 'foo', keys: %w(bar baz))).to raise_error(Symgate::Error)
     end
 
     it 'raises an error if an unknown option is supplied' do
+      client = Symgate::Metadata::Client.new(account: 'foo', user: 'foo/bar', password: 'baz')
+      expect(client.get_metadata(kettle: 'black')).to raise_error(Symgate::Error)
     end
   end
 
