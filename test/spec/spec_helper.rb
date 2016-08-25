eea7ad034d87a197d4316130d7e1a6b14f26b24b
Simon Detheridge <simon@widgit.com>
2016-08-25 15:42:11 +0100

    Add helper method to generate auth:creds objects for savon mocks

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -12,3 +12,13 @@ RSpec.configure do |config|
     savon.unmock!
   end
 end
+
+def account_key_creds(account, key)
+  { 'auth:account': account, 'auth:key': key }
+end
+
+def user_password_creds(account, user, password)
+  { 'auth:account': account,
+    'auth:user': { 'auth:id': user,
+                   'auth:password': password } }
+end
