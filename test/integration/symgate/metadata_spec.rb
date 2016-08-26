ed04cb094e7d8ee5efbfcb6cdf2631744d905175
Simon Detheridge <simon@widgit.com>
2016-08-26 14:56:31 +0100

    Implement remaining integration tests for get_metadata

--- test/integration/symgate/metadata_spec.rb
+++ test/integration/symgate/metadata_spec.rb
@@ -35,8 +35,133 @@ RSpec.describe(Symgate::Metadata::Client) do
     it 'returns an array of one DataItem if there is one item' do
       expect { create_data_item('foo', 'bar', 'User') }.not_to raise_error
       expect(client.get_metadata).to match_array(
-        [Symgate::Metadata::DataItem.new(key: 'foo', 'value': 'bar', scope: 'User')]
+        [
+          Symgate::Metadata::DataItem.new(key: 'foo', 'value': 'bar', scope: 'User')
+        ]
       )
+    end
+
+    it 'returns an array of two DataItems if there are two items' do
+      expect { create_data_item('foo', 'bar', 'User') }.not_to raise_error
+      expect { create_data_item('baz', 'qux', 'User') }.not_to raise_error
+      expect(client.get_metadata).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'foo', 'value': 'bar', scope: 'User'),
+          Symgate::Metadata::DataItem.new(key: 'baz', 'value': 'qux', scope: 'User')
+        ]
+      )
+    end
+
+    it 'returns results limited by scope when a scope is passed' do
+      expect { create_data_item('foo', 'bar', 'User') }.not_to raise_error
+      expect { create_data_item('baz', 'qux', 'Group') }.not_to raise_error
+      expect(client.get_metadata(scope: 'Group')).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'baz', 'value': 'qux', scope: 'Group')
+        ]
+      )
+    end
+
+    it 'returns results limited by key when a key is passed' do
+      expect { create_data_item('foo', 'bar', 'User') }.not_to raise_error
+      expect { create_data_item('baz', 'qux', 'Group') }.not_to raise_error
+      expect(client.get_metadata(key: 'baz')).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'baz', 'value': 'qux', scope: 'Group')
+        ]
+      )
+    end
+
+    it 'returns results limited by multiple keys when multiple keys are passed' do
+      expect { create_data_item('foo', 'bar', 'User') }.not_to raise_error
+      expect { create_data_item('baz', 'qux', 'Group') }.not_to raise_error
+      expect { create_data_item('garply', 'thud', 'Group') }.not_to raise_error
+      expect(client.get_metadata(keys: %w(foo baz))).to match_array(
+        [
+          Symgate::Metadata::DataItem.new(key: 'foo', 'value': 'bar', scope: 'User'),
+          Symgate::Metadata::DataItem.new(key: 'baz', 'value': 'qux', scope: 'Group')
+        ]
+      )
+    end
+
+    it 'raises an error if supplied an invalid scope' do
+      expect { client.get_metadata(scope: 'Teapot') }.to raise_error(Symgate::Error)
+    end
+  end
+
+  describe '#set_metadata' do
+    it 'allows a user to set a user-level permission' do
+
+    end
+
+    it 'allows a group admin user to set a group-level permission' do
+
+    end
+
+    it 'does not allow a non-admin user to set a group-level permission' do
+
+    end
+
+    it 'disallows setting of account-level permissions for users' do
+
+    end
+
+    it 'allows setting of account-level permissions with an account/key' do
+
+    end
+
+    it 'raises an error when setting of user- and group-level permissions with an account/key' do
+
+    end
+
+    it 'overwrites data with the same key and scope when passed one which already exists' do
+
+    end
+
+    it 'creates separate data items for different scopes with the same key' do
+
+    end
+  end
+
+  describe '#destroy_metadata' do
+    it 'raises an error with an invalid scope' do
+
+    end
+
+    it 'deletes user metadata for users when passed a key' do
+
+    end
+
+    it 'deletes multiple metadata items for users when passed multiple keys' do
+
+    end
+
+    it 'does not raise an error when deleting an item that does not exist' do
+
+    end
+
+    it 'only deletes metadata for the specified scope' do
+
+    end
+
+    it 'allows group admins to delete group-level metadata' do
+
+    end
+
+    it 'raises an error when non-group-admins attempt to delete group-level metadata' do
+
+    end
+
+    it 'raises an error when normal users attempt to delete account-level metadata' do
+
+    end
+
+    it 'allows account-level metadata to be deleted with an account/key' do
+
+    end
+
+    it 'raises an error when trying to delete user- or group-level metadata with an account/key' do
+
     end
   end
 end
