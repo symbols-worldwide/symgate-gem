a808ac395b9d28c968f331fb922355cb25d3940e
Tim Down <tim@widgit.com>
2018-10-25 10:36:14 +0100

    SOS-258: Clean up after running all tests and put integration config in one place for easy nadgering

--- test/integration/spec_helper.rb
+++ test/integration/spec_helper.rb
@@ -5,12 +5,24 @@ require 'symgate/wordlist/entry'
 
 # rubocop:disable Naming/AccessorMethodName
 
+def symboliser_config
+  {
+      :db_host => '127.0.0.1',
+      :db_port => 33306,
+      :db_name => 'symboliser',
+      :db_username => 'symboliser',
+      :db_password => 'symboliser',
+      :soap_endpoint => 'http://localhost:11122/'
+  }
+end
+
 def integration_mysql_client
-  Mysql2::Client.new(host: '127.0.0.1',
-                     port: 33306,
-                     username: 'symboliser',
-                     password: 'symboliser',
-                     database: 'symboliser')
+  config = symboliser_config
+  Mysql2::Client.new(host: config[:db_host],
+                     port: config[:db_port],
+                     username: config[:db_username],
+                     password: config[:db_password],
+                     database: config[:db_name])
 end
 
 def savon_opts
@@ -23,12 +35,16 @@ RSpec.configure do |config|
     integration_mysql_client.query('INSERT INTO accounts (account_name,account_key) '\
                                    'VALUES ("integration","x");')
   end
+
+  config.after(:all) do
+    integration_mysql_client.query('DELETE FROM accounts WHERE account_name="integration";')
+  end
 end
 
 def account_key_client_of_type(client_type)
   client_type.new(account: 'integration',
                   key: 'x',
-                  endpoint: 'http://localhost:11122/',
+                  endpoint: symboliser_config[:soap_endpoint],
                   savon_opts: savon_opts)
 end
 
@@ -36,7 +52,7 @@ def user_password_client_of_type(client_type, user, password)
   client_type.new(account: 'integration',
                   user: user,
                   password: password,
-                  endpoint: 'http://localhost:11122/',
+                  endpoint: symboliser_config[:soap_endpoint],
                   savon_opts: savon_opts)
 end
 
@@ -44,7 +60,7 @@ def user_token_client_of_type(client_type, user, token)
   client_type.new(account: 'integration',
                   user: user,
                   token: token,
-                  endpoint: 'http://localhost:11122/',
+                  endpoint: symboliser_config[:soap_endpoint],
                   savon_opts: savon_opts)
 end
 
